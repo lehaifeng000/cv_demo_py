f62697e310c19eb5f4c3882efd3bc5d50d2e9cc7
lehaifeng000 <lehaifeng000@163.com>
2020-09-25 17:18:46 +0800

    测试rgb转灰度图

--- demo0923/t1.py
+++ demo0923/t1.py
@@ -1,4 +1,6 @@
 import cv2
 img = cv2.imread('lena.jpg', cv2.IMREAD_COLOR)
+gray_img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+cv2.imwrite("test.jpg",gray_img)
 cv2.imshow('image', img)
 K = cv2.waitKey(0)
